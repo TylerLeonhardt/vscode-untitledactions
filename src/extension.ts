a2ac72ce0dc421b8461b2d34060f4509c6ec7e4d
Tyler James Leonhardt <me@tylerleonhardt.com>
2021-02-11 19:44:56 +0000

    add decorator

--- src/extension.ts
+++ src/extension.ts
@@ -1,10 +1,15 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import { ExtensionContext, languages, commands, Disposable, workspace, window } from 'vscode';
+import { ExtensionContext, languages, commands, Disposable, workspace, window, TextEditor, DecorationOptions, Range, Position } from 'vscode';
 import { CodelensProvider } from './CodelensProvider';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
+const decorationType = window.createTextEditorDecorationType({
+	after: {
+		color: 'darkgray',
+		fontStyle: 'italic',
+		contentText: '\tOnce you start typing these buttons will go away...'
+	}
+});
 
 export function activate(context: ExtensionContext) {
 	const codelensProvider = new CodelensProvider();
@@ -14,7 +19,37 @@ export function activate(context: ExtensionContext) {
 	context.subscriptions.push(commands.registerTextEditorCommand("vscode-untitledactions.codelensAction", async (textEditor, textEditorEdit) => {
 		await commands.executeCommand('workbench.extensions.action.showExtensionsForLanguage', textEditor.document.languageId);
 	}));
+
+	workspace.onDidChangeTextDocument(event => {
+		const openEditor = window.visibleTextEditors.filter(
+			editor => editor.document.uri === event.document.uri
+		)[0];
+		decorate(openEditor);
+	});
+
+	workspace.onDidOpenTextDocument(document => {
+		const openEditor = window.visibleTextEditors.filter(
+			editor => editor.document.uri === document.uri
+		)[0];
+		decorate(openEditor);
+	});
+
+	window.visibleTextEditors.forEach(e => decorate(e));
+}
+
+function decorate(editor: TextEditor) {
+	let sourceCode = editor.document.getText();
+
+	let decorationsArray: DecorationOptions[] = [];
+
+	if (!sourceCode) {
+		decorationsArray.push({
+			range: new Range(0,0,0,0)
+		});
+	}
+
+	editor.setDecorations(decorationType, decorationsArray);
 }
 
 // this method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() { }
